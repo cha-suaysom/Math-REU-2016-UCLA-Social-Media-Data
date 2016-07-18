e86621152702923cb555dc0a3129b00d30a5ec5c
dukleryoi <dukleryoni@gmail.com>
2016-07-18 09:43:06 -0700

    New changelist

--- generate_topics.py
+++ generate_topics.py
@@ -1,13 +1,14 @@
 import pickle
 import numpy as np
 
-(W, H) = pickle.load(open('Location_NMF_100_topics_barc_WH.pkl','rb'))
-names = np.array(pickle.load(open('TF_IDF_feature_names_barc.pkl','rb')))
-
+(W, H) = pickle.load(open('Location_NMF_100_topics_vanc_WH.pkl','rb'))
+names = np.array(pickle.load(open('TF_IDF_feature_names_vanc.pkl','rb')))
+rows = 100
+cols = 180
 print(H.shape)
-print(names[:500])
+print(names[:100])
 print(H[:20,:20])
-H1 = H[:,:-10000]
+H1 = H[:,:-rows*cols]
 sorted = [list(names[np.argsort(x)[-10:]][::-1]) for x in H1] # get the indices of the 10 highest values in each topic in H, then get the corresponding words for these values
 for idx, s in enumerate(sorted):
     print(idx,s)
